7a1f3fea30c1b49932c1a6ae45b7b795f13fc355
cristobal-1 <cristobal1@gmail.com>
2022-12-09 05:08:08 -0300

    adding grid

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
   return (
     <div className="App">
     <NavBar></NavBar>
-    <CardProducts></CardProducts>
-
+    <Products></Products>
     </div>
   );
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,98 +1,38 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { AddShoppingCart } from '@mui/icons-material';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import CardProducts from './Card';
 import { makeStyles } from '@mui/styles';
-import clsx from 'clsx';
 
-const useStyles = makeStyles((theme) => ({
-  root:{
-    maxWidth: 345,
-  },
-  action:{
-    marginTop:"10rem",
-  },
-  media:{
-    height:194,
-    paddingTop:"56%"
-  },
-  expand:{
-    transform:"rotate(0deg)",
-    marginLeft:"auto",
-    transition:theme.transition.create("transform",{
-      duration: theme.transition.duration.shortest,
-    }),
-  },
-  expandOpen:{
-    transform:"rotate(180deg)",
-  },
-}));
-
+const useStyles = makeStyles((theme)=>({
+    root:{
+        flexGrow:1,
+        padding: '2rem'
+    },
+    }));
 
 export default function Products() {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
+    const classes = useStyles();
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
   return (
-    <Card className={classes.root}>
-      <CardHeader
-        action={
-          <Typography
-            className={classes.action}
-            variant='h5'
-            color='textSecondary'>
-            {50}
-          </Typography>
-        }
-        title="Coffe Arabic"
-        subheader="In Stock"
-      />
-      <CardMedia
-        component="img"
-        className={classes.media}
-        image="https://st2.depositphotos.com/3944627/7940/i/450/depositphotos_79404590-stock-photo-white-coffee-cup-and-coffee.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-        like snow
-        </Typography>
-      </CardContent>
-      <CardActions disableSpac ing>
-        <IconButton aria-label="add to favorites">
-          <AddShoppingCart fontSize='large'></AddShoppingCart>
-        </IconButton>
-        {Array(4)
-        .fill()
-        .map((_,i)=>(<p>&#11088;</p>
-        )
-        )}
-        <IconButton
-          className={clsx(classes.expand,{
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-         <Typography paragraph> Coffe is screem</Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    <Box className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <CardProducts></CardProducts>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <CardProducts></CardProducts>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <CardProducts></CardProducts>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <CardProducts></CardProducts>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <CardProducts></CardProducts>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
